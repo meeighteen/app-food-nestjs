31046d07506a6d9a34b414ed83ad136cbc238106
Josue <josuee.ald@gmail.com>
2024-01-18 07:08:20 -0500

    edit gitignore

--- src/business/business.entity.ts
+++ src/business/business.entity.ts
@@ -1,23 +1,23 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
-import { Owner } from '../owner/owner.entity';
+// import { Entity, Column, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
+// import { Owner } from '../owner/owner.entity';
 
-@Entity()
-export class Business {
-  @PrimaryGeneratedColumn()
-  _id: number;
+// @Entity()
+// export class Business {
+//   @PrimaryGeneratedColumn()
+//   _id: number;
 
-  @Column()
-  name: string;
+//   @Column()
+//   name: string;
 
-  @Column()
-  description: string;
+//   @Column()
+//   description: string;
 
-  @Column()
-  owner: string;
+//   @Column()
+//   owner: string;
 
-  @Column({ default: true })
-  isActive: boolean;
+//   @Column({ default: true })
+//   isActive: boolean;
 
-  @OneToOne(() => Owner, (owner) => owner._id)
-  ownerId: Owner;
-}
+//   @OneToOne(() => Owner, (owner) => owner._id)
+//   ownerId: Owner;
+// }
